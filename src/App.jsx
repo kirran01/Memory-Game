52a7d7495121253c4ae2e4b962a242850fcaf16f
kirran01 <Kirran-@live.com>
2023-01-24 13:02:24 -0400

    1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  [{a:1},{b:2},{c:3}]
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
