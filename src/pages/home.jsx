c7972ca52a95e1c757bea803771685427570ec58
kirran01 <Kirran-@live.com>
2023-01-24 15:51:04 -0400

    flipping

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,10 +4,28 @@ import { useState } from 'react';
 
 
 const Home = ({ cards, setCards }) => {
+    function startGame() {
+        console.log('startgame')
+    }
+
+    const flipCards = () => {
+        console.log('flip')
+        const flipped = cards.map(card => {
+            return { ...card, isFlipped: true }
+        })
+        setCards(flipped)
+        setTimeout(() => {
+            const flippedBack = cards.map(card => {
+                return { ...card, isFlipped: false }
+            })
+            setCards(flippedBack)
+        }, 3000);
+    }
+
     return (
         <div className='flex flex-col justify-center items-center content-center bg-indigo-300 text-white'>
-            <h1 className='text-center text-xl'>Memory Game</h1>
-            <button className='bg-indigo-900 px-10 py-5 rounded-lg m-5'>Start</button>
+            <h1 className='text-center text-5xl m-5'>Memory Game</h1>
+            <button onClick={flipCards} className='bg-indigo-900 px-10 py-5 rounded-lg m-5'>Start</button>
             <div className='flex flex-wrap justify-center'>
                 {
                     cards.map(card => {
