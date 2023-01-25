bae5bcd1577fe6b0a709e2585e84f0e2241d3db4
kirran01 <Kirran-@live.com>
2023-01-24 23:28:59 -0400

    flipping card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,11 +3,24 @@ import { useState } from 'react';
 
 const Card = ({ card, cards, setCards }) => {
     const [cardImg, setCardImg] = useState(card.imgURL)
+    console.log(card, 'c')
+    console.log(card.isFlipped, 'cif')
+    const flipCard = () => {
+        const updatedCards = cards.map(oneCard => {
+            if (oneCard.id === card.id) {
+                return { ...oneCard, isFlipped: true }
+            } else {
+                return oneCard
+            }
+        })
+        setCards(updatedCards)
+    }
     return (
-        <div className='flex justify-center border-8 border-double shadow-md m-5 bg-white border-indigo-100 rounded-md w-60 cursor-pointer'>
+        <div onClick={() => flipCard()} className='flex justify-center border-8 border-double shadow-md m-5 bg-white border-indigo-100 rounded-md w-60 cursor-pointer'>
             <img className='h-40 m-5' src={card.isFlipped ? `../${cardImg}` : <></>} alt="" />
         </div>
     );
+
 }
 
 export default Card;
