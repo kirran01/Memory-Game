ac8b5f95bc9075c68402a3b66aebde24552fdd9c
kirran01 <Kirran-@live.com>
2023-01-30 16:24:07 -0400

    importing images instead of routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,23 @@ import Card from './components/card'
 import Nav from './components/nav'
 import Foot from './components/foot'
 import Home from './pages/home'
+import rubyIcon from '../public/img/ruby.png'
+import ponyoIcon from '../public/img/ponyo.png'
+import mushIcon from '../public/img/mush.png'
+import poroIcon from '../public/img/poro.png'
 
 function App() {
-  const [cards, setCards] = useState([{ id: 1, name: 'ruby', isFlipped: false, isMatched:false, imgURL: '../public/img/ruby.png' }, { id: 2, name: 'ruby', isFlipped: false, isMatched:false, imgURL: '../public/img/ruby.png' }, { id: 3, name: 'ponyo', isFlipped: false, isMatched:false, imgURL: '../public/img/ponyo.png' }, { id: 4, name: 'ponyo', isFlipped: false, isMatched:false, imgURL: '../public/img/ponyo.png' }, { id: 5, name: 'mushroom', isFlipped: false, isMatched:false, imgURL: '../public/img/mush.png' }, { id: 6, name: 'mushroom', isFlipped: false, isMatched:false, imgURL: '../public/img/mush.png' },{ id: 7, name: 'poro', isFlipped: false, isMatched:false, imgURL: '../public/img/poro.png' },{ id: 8, name: 'poro', isFlipped: false, isMatched:false, imgURL: '../public/img/poro.png' }])
+
+  const [cards, setCards] = useState([
+    { id: 1, name: 'ruby', isFlipped: false, isMatched: false, imgURL: rubyIcon },
+    { id: 2, name: 'ruby', isFlipped: false, isMatched: false, imgURL: rubyIcon },
+    { id: 3, name: 'ponyo', isFlipped: false, isMatched: false, imgURL: ponyoIcon },
+    { id: 4, name: 'ponyo', isFlipped: false, isMatched: false, imgURL: ponyoIcon },
+    { id: 5, name: 'mushroom', isFlipped: false, isMatched: false, imgURL: mushIcon },
+    { id: 6, name: 'mushroom', isFlipped: false, isMatched: false, imgURL: mushIcon },
+    { id: 7, name: 'poro', isFlipped: false, isMatched: false, imgURL: poroIcon },
+    { id: 8, name: 'poro', isFlipped: false, isMatched: false, imgURL: poroIcon }])
+
   return (
     <div className="App">
       <Routes>
