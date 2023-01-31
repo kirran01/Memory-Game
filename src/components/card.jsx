5c96879507c7d22123f85889f7d1ed61a1d07d4c
kirran01 <Kirran-@live.com>
2023-01-31 13:09:58 -0400

    breakpoint tests

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -16,8 +16,8 @@ const Card = ({ card, cards, setCards, setCount, victory,count }) => {
         setCount((previousState) => previousState + 1)
     }
     return (
-        <div onClick={() => flipCard()} className={`${card.isMatched || victory===false? 'bg-transparent border-none' : 'bg-white border-indigo-100 border-8 border-double shadow-md'} flex justify-center m-5 rounded-md w-60 cursor-pointer`}>
-            <img className='h-40 m-5' src={card.isFlipped ? `../${cardImg}` : <></>} alt="" />
+        <div onClick={() => flipCard()} className={`${card.isMatched || victory===false? 'bg-transparent border-none' : 'bg-white border-indigo-100 border-8 border-double shadow-md'} flex justify-center m-5 rounded-md w-32 md:w-60 lg:w-60 cursor-pointer`}>
+            <img className='h-8 md:h-40 lg:h-40 m-5' src={card.isFlipped ? `../${cardImg}` : <></>} alt="" />
         </div>
     );
 
