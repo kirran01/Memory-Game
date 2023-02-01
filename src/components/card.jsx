ca0fe539eb70b26ea54aa41f9000c53a7f360c70
kirran01 <Kirran-@live.com>
2023-02-01 14:55:06 -0400

    double click bug

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,17 +3,17 @@ import { useState } from 'react';
 
 const Card = ({ card, cards, setCards, setCount, victory,count }) => {
     const [cardImg, setCardImg] = useState(card.imgURL)
-    const [matched, setMatched] = useState(false)
     const flipCard = () => {
         const updatedCards = cards.map(oneCard => {
-            if (count<2&&victory==null&&!oneCard.isMatched && oneCard.id === card.id) {
+            if (oneCard.isFlipped===false&&count<2&&victory==null&&!oneCard.isMatched && oneCard.id === card.id) {
+                setCount((previousState) => previousState + 1)
                 return { ...oneCard, isFlipped: true }
             } else {
                 return oneCard
             }
         })
         setCards(updatedCards)
-        setCount((previousState) => previousState + 1)
+
     }
     return (
         <div onClick={() => flipCard()} className={`${card.isMatched || victory===false? 'bg-transparent border-none' : 'bg-white border-indigo-100 border-8 border-double shadow-md'} flex justify-center m-5 rounded-md w-20 md:w-60 lg:w-60 cursor-pointer`}>
