81011c5ad501616176485aa09e37ef3c6e6a1b79
kirran01 <Kirran-@live.com>
2023-01-31 23:40:33 -0400

    ui

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -17,7 +17,7 @@ const Card = ({ card, cards, setCards, setCount, victory,count }) => {
     }
     return (
         <div onClick={() => flipCard()} className={`${card.isMatched || victory===false? 'bg-transparent border-none' : 'bg-white border-indigo-100 border-8 border-double shadow-md'} flex justify-center m-5 rounded-md w-32 md:w-60 lg:w-60 cursor-pointer`}>
-            <img className='h-8 md:h-40 lg:h-40 m-5' src={card.isFlipped ? `../${cardImg}` : <></>} alt="" />
+            <img className='h-8 mx-0 my-5 md:h-40 lg:h-40 lg:m-5 md:m-5' src={card.isFlipped ? `../${cardImg}` : <></>} alt="" />
         </div>
     );
 
